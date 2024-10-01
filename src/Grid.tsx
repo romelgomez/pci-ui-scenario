8b680e130cf6d85fb19abb1ca0bbe12d3647b9c4
romelgomez <bmxandcode@gmail.com>
2024-10-01 10:04:49 -0500

    feat: Task #4: Updated Potentially Hazardous column to display 'Yes', 'No', and blank.

--- src/Grid.tsx
+++ src/Grid.tsx
@@ -19,7 +19,13 @@ const columnDefs: ColDef[] = [
   { field: "q_au_2", headerName: "Q (au)", sortable: true, filter: 'agNumberColumnFilter' },
   { field: "period_yr", headerName: "Period (yr)", sortable: true, filter: 'agNumberColumnFilter' },
   { field: "i_deg", headerName: "Inclination (deg)", sortable: true, filter: 'agNumberColumnFilter' },
-  { field: "pha", headerName: "Potentially Hazardous", sortable: true, filter: 'agTextColumnFilter' },
+  {
+    field: "pha",
+    headerName: "Potentially Hazardous",
+    sortable: true,
+    filter: 'agTextColumnFilter',
+    valueFormatter: (params) => params.value === 'Y' ? 'Yes' : params.value === 'N' ? 'No' : '',
+  },
   { field: "orbit_class", headerName: "Orbit Class", sortable: true, filter: 'agTextColumnFilter' },
 ];
 
