b7e8992df28a66833ea1930beff447955adc966d
romelgomez <bmxandcode@gmail.com>
2024-10-01 09:45:18 -0500

    feat: Task #2: Enabled sorting and filtering for all columns.

--- src/Grid.tsx
+++ src/Grid.tsx
@@ -5,16 +5,16 @@ import "ag-grid-community/styles/ag-grid.css";
 import "ag-grid-community/styles/ag-theme-alpine.css";
 
 const columnDefs: ColDef[] = [
-  { field: "designation", headerName: "Designation" },
-  { field: "discovery_date", headerName: "Discovery Date" },
-  { field: "h_mag", headerName: "H (mag)" },
-  { field: "moid_au", headerName: "MOID (au)" },
-  { field: "q_au_1", headerName: "q (au)" },
-  { field: "q_au_2", headerName: "Q (au)" },
-  { field: "period_yr", headerName: "Period (yr)" },
-  { field: "i_deg", headerName: "Inclination (deg)" },
-  { field: "pha", headerName: "Potentially Hazardous" },
-  { field: "orbit_class", headerName: "Orbit Class", enableRowGroup: true, },
+  { field: "designation", headerName: "Designation", sortable: true, filter: 'agTextColumnFilter' },
+  { field: "discovery_date", headerName: "Discovery Date", sortable: true, filter: 'agDateColumnFilter' },
+  { field: "h_mag", headerName: "H (mag)", sortable: true, filter: 'agNumberColumnFilter' },
+  { field: "moid_au", headerName: "MOID (au)", sortable: true, filter: 'agNumberColumnFilter' },
+  { field: "q_au_1", headerName: "q (au)", sortable: true, filter: 'agNumberColumnFilter' },
+  { field: "q_au_2", headerName: "Q (au)", sortable: true, filter: 'agNumberColumnFilter' },
+  { field: "period_yr", headerName: "Period (yr)", sortable: true, filter: 'agNumberColumnFilter' },
+  { field: "i_deg", headerName: "Inclination (deg)", sortable: true, filter: 'agNumberColumnFilter' },
+  { field: "pha", headerName: "Potentially Hazardous", sortable: true, filter: 'agTextColumnFilter' },
+  { field: "orbit_class", headerName: "Orbit Class", sortable: true, filter: 'agTextColumnFilter' },
 ];
 
 const NeoGrid = (): JSX.Element => {
@@ -24,6 +24,10 @@ const NeoGrid = (): JSX.Element => {
         rowData={data}
         columnDefs={columnDefs}
         rowGroupPanelShow={'always'}
+        defaultColDef={{
+          sortable: true,
+          filter: true,
+        }}
       />
     </div>
   );
