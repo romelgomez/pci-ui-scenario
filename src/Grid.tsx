3c2655d92ea89c6b27d1b1518220e53ec8ed3576
romelgomez <bmxandcode@gmail.com>
2024-10-01 09:52:59 -0500

    feat: Task #3: Formatted discovery_date using JavaScript's built-in date formatting.

--- src/Grid.tsx
+++ src/Grid.tsx
@@ -6,7 +6,13 @@ import "ag-grid-community/styles/ag-theme-alpine.css";
 
 const columnDefs: ColDef[] = [
   { field: "designation", headerName: "Designation", sortable: true, filter: 'agTextColumnFilter' },
-  { field: "discovery_date", headerName: "Discovery Date", sortable: true, filter: 'agDateColumnFilter' },
+  {
+    field: "discovery_date",
+    headerName: "Discovery Date",
+    sortable: true,
+    filter: 'agDateColumnFilter',
+    valueFormatter: (params) => new Date(params.value).toLocaleDateString('en-US', { year: 'numeric', month: 'long', day: 'numeric' }),
+  },
   { field: "h_mag", headerName: "H (mag)", sortable: true, filter: 'agNumberColumnFilter' },
   { field: "moid_au", headerName: "MOID (au)", sortable: true, filter: 'agNumberColumnFilter' },
   { field: "q_au_1", headerName: "q (au)", sortable: true, filter: 'agNumberColumnFilter' },
